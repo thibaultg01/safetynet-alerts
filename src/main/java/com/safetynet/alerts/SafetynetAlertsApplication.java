6acfa323834940a12343a5fb97ca5e0021c8c166
thibaultg01 <thi.bault.guittoun@gmail.com>
2025-04-15 17:39:46 +0200

    add spring boot project structure

---
+++ src/main/java/com/safetynet/alerts/SafetynetAlertsApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.alerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetynetAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetynetAlertsApplication.class, args);
+	}
+
+}
